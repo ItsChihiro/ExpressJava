ff03b15e0282fd96b9e3141a9a868fbd8e1f29db
maryam <maryam.shakirova.2017@mail.ru>
2026-02-03 21:51:44 +0200

    practice 4 part 1

--- src/main/java/homework_3/LibraryTest.java
+++ src/main/java/homework_3/LibraryTest.java
@@ -3,7 +3,8 @@ package homework_3;
 public class LibraryTest {
     public static void main(String[] args) {
         Library library1 = new Library();
-//        System.out.println(library1.bookTitle);
+        // bookTitle is private and not accessible here
+        // System.out.println(library1.bookTitle);
         System.out.println(library1.author);
         System.out.println(library1.year);
         System.out.println(library1.category);
