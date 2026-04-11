6ff67b73a1037a94ddf8273df20a15912874364a
maryam <maryam.shakirova.2017@mail.ru>
2026-04-11 21:28:10 +0300

    task 3 contact_book

---
+++ src/main/java/practice_6/contact_book/Main.java
@@ -0,0 +1,14 @@
+package practice_6.contact_book;
+
+public class Main {
+    static void main(String[] args) {
+        ContactBook contactBook = new ContactBook();
+        contactBook.addContact("Sam", 1233);
+        contactBook.addContact("Luk", 1234);
+
+        contactBook.printContacts();
+
+        contactBook.updatePhone("Luk", 2563);
+        contactBook.printContacts();
+    }
+}
