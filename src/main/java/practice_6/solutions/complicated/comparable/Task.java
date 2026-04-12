6123be1070faa47582e8ca14e7e267c13fe9853e
maryam <maryam.shakirova.2017@mail.ru>
2026-04-12 13:21:58 +0300

    task manager

---
+++ src/main/java/practice_6/solutions/complicated/comparable/Task.java
@@ -0,0 +1,41 @@
+package practice_6.solutions.complicated.comparable;
+
+import java.util.Date;
+
+public class Task implements Comparable<Task> {
+    private String name;
+    private Date date;
+
+    public Task(String name, Date date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "name='" + name + '\'' +
+                ", date=" + date +
+                '}';
+    }
+
+    /**
+     * Возвращаем негативное число, если задача task более приоритетная (то есть дедлайн раньше чем у текущей задачи this)
+     * Возвращаем 0, если задача task такого же приоритета (то есть дедлайны равны с this)
+     * Возвращаем позитивное число, если задача task менее приоритетная (то есть дедлайн позже чем у текущей задачи this)
+     * @param task the object to be compared.
+     * @return
+     */
+    @Override
+    public int compareTo(Task task) {
+        return this.date.compareTo(task.date);
+    }
+}
