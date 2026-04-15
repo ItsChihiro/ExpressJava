76ed8c4195477b9f01aab140063d664c9966c45b
maryam <maryam.shakirova.2017@mail.ru>
2026-04-15 19:31:48 +0300

    2 - LinkedList  (2)

--- src/main/java/homework_6/linked_list/Main.java
+++ src/main/java/homework_6/linked_list/Main.java
@@ -1,4 +1,34 @@
 package homework_6.linked_list;
 
+import java.util.LinkedList;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task 1.");
+
+        LinkedList<String> listOfStrings = new LinkedList<>();
+
+        listOfStrings.add("one");
+        listOfStrings.add("two");
+        listOfStrings.add("three");
+        listOfStrings.add("four");
+        listOfStrings.add("five");
+
+        System.out.println("Five strings: ");
+        for (String s: listOfStrings) {
+            System.out.println(s);
+        }
+
+
+        System.out.println("Task 2.");
+
+        LinkedList<String> tasksQueue = new LinkedList<>();
+
+        tasksQueue.add("Go to gym");
+        tasksQueue.add("Do homework");
+        tasksQueue.add("Make an order");
+
+        System.out.println("3 tasks: ");
+        while (!tasksQueue.isEmpty()) System.out.println(tasksQueue.removeFirst());
+    }
 }
