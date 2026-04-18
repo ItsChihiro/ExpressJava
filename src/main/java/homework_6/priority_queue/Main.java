b3377c877f82bca311576368f39c0db7f1c1a184
maryam <maryam.shakirova.2017@mail.ru>
2026-04-18 21:55:43 +0300

    9 PriorityQueue (1)

--- src/main/java/homework_6/priority_queue/Main.java
+++ src/main/java/homework_6/priority_queue/Main.java
@@ -1,4 +1,20 @@
 package homework_6.priority_queue;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Main {
+    static void main(String[] args) {
+        System.out.println("Task 1.");
+
+        Queue<Integer> priorityQueue = new PriorityQueue<>();
+
+        priorityQueue.offer(5);
+        priorityQueue.offer(2);
+        priorityQueue.offer(3);
+        priorityQueue.offer(1);
+        priorityQueue.offer(4);
+
+        while (!priorityQueue.isEmpty()) System.out.println(priorityQueue.poll());
+    }
 }
