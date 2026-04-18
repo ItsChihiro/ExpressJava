46f8af13623352a51730caea504b5aa59e5b1eb8
maryam <maryam.shakirova.2017@mail.ru>
2026-04-18 20:55:07 +0300

    5 TreeSet (1)

--- src/main/java/homework_6/tree_set/Main.java
+++ src/main/java/homework_6/tree_set/Main.java
@@ -1,4 +1,24 @@
 package homework_6.tree_set;
 
+import java.util.TreeSet;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task 3.");
+
+        TreeSet<Integer> treeSet = new TreeSet<>();
+
+        treeSet.add(10);
+        treeSet.add(11);
+        treeSet.add(12);
+        treeSet.add(13);
+
+        Integer target = 13;
+
+        Integer higher = treeSet.higher(target);
+        Integer lower =  treeSet.lower(target);
+
+        if (higher != null) System.out.println("The closest big number to " + target + ": " + higher);
+        if (lower != null) System.out.println("The closest small number to " + target + ": " + lower);
+    }
 }
