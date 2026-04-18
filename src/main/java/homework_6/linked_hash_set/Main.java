9e565be5b59eeea737c14920f0f9c163296fd3b6
maryam <maryam.shakirova.2017@mail.ru>
2026-04-18 20:02:49 +0300

    4 LinkedHashSet (1) change

--- src/main/java/homework_6/linked_hash_set/Main.java
+++ src/main/java/homework_6/linked_hash_set/Main.java
@@ -1,22 +1,21 @@
 package homework_6.linked_hash_set;
 
 import java.util.LinkedHashSet;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task 2.");
 
-        Set<String> stringSet = new LinkedHashSet<>();
+        LinkedHashSet<String> stringSet = new LinkedHashSet<>();
 
-        addUniqueElements(stringSet, "one");
-        addUniqueElements(stringSet, "two");
-        addUniqueElements(stringSet, "one");
-        addUniqueElements(stringSet, "three");
+        addUniqueString(stringSet, "one");
+        addUniqueString(stringSet, "two");
+        addUniqueString(stringSet, "one");
+        addUniqueString(stringSet, "three");
     }
 
 
-    public static void addUniqueElements(Set<String> set, String element) {
+    public static void addUniqueString(LinkedHashSet<String> set, String element) {
         if (set.contains(element)) {
             System.out.println(element + " is not unique string, nothing changed: " + set);
         } else {
