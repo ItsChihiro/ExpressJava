5d86b3fc4c6eaa6d24a2de4bcc37034b7a0502b8
maryam <maryam.shakirova.2017@mail.ru>
2026-04-18 21:42:33 +0300

    7 TreeMap (1)

--- src/main/java/homework_6/linked_hash_map/Main.java
+++ src/main/java/homework_6/linked_hash_map/Main.java
@@ -1,4 +1,27 @@
 package homework_6.linked_hash_map;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task 2.");
+
+        Map<String, String> contacts = new LinkedHashMap<>();
+
+        contacts.put("Eva", "12345");
+        contacts.put("Sam", "22345");
+        contacts.put("Nick", "32345");
+        contacts.put("Mark", "42345");
+
+        findPhoneNumber(contacts, "Eva");
+        findPhoneNumber(contacts, "Nicky");
+    }
+
+
+    public static void findPhoneNumber(Map<String, String> contactBook, String name) {
+        if (contactBook.containsKey(name)) {
+            System.out.println(name + "'s phone number: " + contactBook.get(name));
+        } else System.out.println(name + "'s phone number is not found.");
+    }
 }
